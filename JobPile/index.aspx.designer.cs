1345f71190b58ce13ddec162ac9a5d2e48d0be49
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-02-27 22:34:36 +0800

    Index.aspx
    SearchBTN shows login Modal

--- JobPile/index.aspx.designer.cs
+++ JobPile/index.aspx.designer.cs
@@ -184,14 +184,5 @@ namespace JobPile
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtSearch;
-
-        /// <summary>
-        /// search control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button search;
     }
 }
