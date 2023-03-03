db4e33ec438d59a0a4240107eaf82a661b2545ad
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-03-03 10:07:32 +0800

    Design

--- JobPile/Employee/EmployeeJob.aspx.designer.cs
+++ JobPile/Employee/EmployeeJob.aspx.designer.cs
@@ -33,15 +33,6 @@ namespace JobPile
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
-        /// empGridView control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView empGridView;
-
-        /// <summary>
         /// appliedbtn control.
         /// </summary>
         /// <remarks>
@@ -49,5 +40,14 @@ namespace JobPile
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button appliedbtn;
+
+        /// <summary>
+        /// empGridView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView empGridView;
     }
 }
